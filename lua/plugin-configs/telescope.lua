50ea6963c7d069b60d86ea12da078bdf48229475
Simon McLean <simonmclean@OVO5828MB.local>
2021-04-06 11:16:23 +0100

    map <c-s> to telescope lsp_document_symbols

--- lua/plugin-configs/telescope.lua
+++ lua/plugin-configs/telescope.lua
@@ -2,3 +2,4 @@ local map = vim.api.nvim_set_keymap
 
 map('n', '<c-p>', ':Telescope find_files<cr>', {})
 map('n', '<c-f>', ':Telescope live_grep<cr>', {})
+map('n', '<c-s>', ':Telescope lsp_document_symbols<cr>', {})
