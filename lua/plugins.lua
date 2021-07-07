673a228b7cb2bef3bf66f05bcf44bd50b0ceaad7
Simon McLean <simonmclean@OVO5828MB.local>
2021-07-07 11:04:04 +0100

    formatting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,65 +11,65 @@ end
 vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
 
 return require('packer').startup(function(use)
-	-- Git
-	use 'airblade/vim-gitgutter'
-	use 'tpope/vim-fugitive'
-	use 'tpope/vim-rhubarb'
-	-- Better editing
-	use 'bkad/camelcasemotion'
-	use 'mattn/emmet-vim'
-	use 'tpope/vim-commentary'
-	use 'tpope/vim-repeat'
-	use 'tpope/vim-surround'
-	use 'windwp/nvim-autopairs'
-	-- UI
-	use 'itchyny/lightline.vim'
-	-- use 'vim-airline/vim-airline'
-	-- use 'vim-airline/vim-airline-themes'
-	use {
-		'liuchengxu/vim-which-key',
-		cmd = 'WhichKey'
-	}
-	use 'lukas-reineke/indent-blankline.nvim'
-	use '~/code/tryptic'
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
-	}
-	-- LSP
-	use 'neovim/nvim-lspconfig'
-	use 'kabouzeid/nvim-lspinstall'
-	use 'hrsh7th/nvim-compe'
-	use {
-		'RishabhRD/nvim-lsputils',
-		requires = {{'RishabhRD/popfix'}}
-	}
-	-- use { 'neoclide/coc.nvim', branch = 'release' }
-	-- DEBUGGING
-	use 'mfussenegger/nvim-dap'
-	-- Tree Sitter
-	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-	-- Language support
-	-- use 'sheerun/vim-polyglot'
-	use 'mustache/vim-mustache-handlebars'
-	use {
-		'styled-components/vim-styled-components',
-		branch = 'main'
-	}
-	use 'scalameta/nvim-metals'
-	use 'vim-scripts/svg.vim'
-	-- Utils
-	use 'heavenshell/vim-jsdoc'
-	use 'tpope/vim-eunuch'
-	use 'tpope/vim-sensible'
-	use 'tpope/vim-sleuth'
-	-- Themes
-	use 'mhartington/oceanic-next'
-	use {"npxbr/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}} -- doesn't yet support airline
-	use 'kyazdani42/blue-moon'
-	use 'bluz71/vim-nightfly-guicolors'
-	use 'sainnhe/sonokai'
-	use 'shaunsingh/moonlight.nvim'
-	use 'tjdevries/colorbuddy.vim'
-	use 'bkegley/gloombuddy'
+  -- Git
+  use 'airblade/vim-gitgutter'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-rhubarb'
+  -- Better editing
+  use 'bkad/camelcasemotion'
+  use 'mattn/emmet-vim'
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-surround'
+  use 'windwp/nvim-autopairs'
+  -- UI
+  use 'itchyny/lightline.vim'
+  -- use 'vim-airline/vim-airline'
+  -- use 'vim-airline/vim-airline-themes'
+  use {
+    'liuchengxu/vim-which-key',
+    cmd = 'WhichKey'
+  }
+  use 'lukas-reineke/indent-blankline.nvim'
+  use '~/code/tryptic'
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  }
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+  use 'kabouzeid/nvim-lspinstall'
+  use 'hrsh7th/nvim-compe'
+  use {
+    'RishabhRD/nvim-lsputils',
+    requires = {{'RishabhRD/popfix'}}
+  }
+  -- use { 'neoclide/coc.nvim', branch = 'release' }
+  -- DEBUGGING
+  use 'mfussenegger/nvim-dap'
+  -- Tree Sitter
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  -- Language support
+  -- use 'sheerun/vim-polyglot'
+  use 'mustache/vim-mustache-handlebars'
+  use {
+    'styled-components/vim-styled-components',
+    branch = 'main'
+  }
+  use 'scalameta/nvim-metals'
+  use 'vim-scripts/svg.vim'
+  -- Utils
+  use 'heavenshell/vim-jsdoc'
+  use 'tpope/vim-eunuch'
+  use 'tpope/vim-sensible'
+  use 'tpope/vim-sleuth'
+  -- Themes
+  use 'mhartington/oceanic-next'
+  use {"npxbr/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}} -- doesn't yet support airline
+  use 'kyazdani42/blue-moon'
+  use 'bluz71/vim-nightfly-guicolors'
+  use 'sainnhe/sonokai'
+  use 'shaunsingh/moonlight.nvim'
+  use 'tjdevries/colorbuddy.vim'
+  use 'bkegley/gloombuddy'
 end)
