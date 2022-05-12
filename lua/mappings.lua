3bf07b4707c1f7793140312891700d4a7db65d39
Simon McLean <simonmclean@OVO-FVFC3065L416.local>
2022-05-12 15:51:25 +0100

    use vim.keymap.set api for mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,33 +1,32 @@
-local map = vim.api.nvim_set_keymap
-local noremap = {noremap=true}
-local silent_noremap = {noremap=true, silent=true}
+local map = vim.keymap.set
+local silent = { silent = true }
 
 -- Leader
 vim.g.mapleader = " "
 
 -- Window control
-map('n', '|', ':vertical split<cr>', silent_noremap)
-map('n', '-', ':split<cr>', silent_noremap)
+map('n', '|', ':vertical split<cr>', silent)
+map('n', '-', ':split<cr>', silent)
 
 -- Lazy exec mode
-map('n', ';', ':', noremap)
-map('v', ';', ':', noremap)
+map('n', ';', ':')
+map('v', ';', ':')
 
 -- Jumps
-map('n', '[q', ':cprevious<cr>', {})
-map('n', ']q', ':cnext<cr>', {})
-map('n', '[b', ':bprevious<cr>', silent_noremap)
-map('n', ']b', ':bnext<cr>', silent_noremap)
+map('n', '[q', ':cprevious<cr>')
+map('n', ']q', ':cnext<cr>')
+map('n', '[b', ':bprevious<cr>')
+map('n', ']b', ':bnext<cr>')
 
 -- Toggle between 2 buffers
-map('n', '<leader><leader>', '<c-^>', noremap)
+map('n', '<leader><leader>', '<c-^>', silent)
 
 -- Add empty line above or below cursor
-map('n', '<leader>k', ':call append(line(".")-1, "")<cr>', silent_noremap)
-map('n', '<leader>j', ':call append(line("."), "")<cr>', silent_noremap)
+map('n', '<leader>k', ':call append(line(".")-1, "")<cr>', silent)
+map('n', '<leader>j', ':call append(line("."), "")<cr>', silent)
 
 -- Replace motion
-map('n', '<leader>p', ':set operatorfunc=ReplaceMotion<cr>g@', silent_noremap)
+map('n', '<leader>p', ':set operatorfunc=ReplaceMotion<cr>g@', silent)
 
 -- Tryptic
 -- map('n', '<leader>-', ':Tryptic<cr>', silent_noremap)
