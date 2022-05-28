60c904e4c83cd53d39dbd05cb4e2c8c781848bfc
Simon McLean <simonmclean@OVO-FVFC3065L416.local>
2022-05-28 11:15:45 +0100

    additional language servers

--- lua/lsp/lsp-config.lua
+++ lua/lsp/lsp-config.lua
@@ -37,7 +37,17 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'tsserver', 'sumneko_lua' }
+local servers = {
+  'tsserver',
+  'sumneko_lua',
+  'graphql',
+  'vimls',
+  'remark_ls',
+  'jsonls',
+  'html',
+  'bashls',
+  'dockerls'
+}
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
