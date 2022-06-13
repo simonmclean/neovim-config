00f9b52aa9b590d128ff57b43f6d598444e3fb49
Simon McLean <simonmclean@OVO-FVFC3065L416.local>
2022-06-13 20:22:39 +0100

    configuring persisted

--- lua/plugin-configs/persisted.lua
+++ lua/plugin-configs/persisted.lua
@@ -9,20 +9,39 @@ local ignored_dirs = {
   'node_modules'
 }
 
+-- function Buffers_to_clear()
+--   local tabs = vim.api.nvim_list_tabpages()
+--   local windows = {}
+--   local visible_buffers = {}
+--   for t = 1, #tabs, 1 do
+--     local tab_windows = vim.api.nvim_tabpage_list_wins(tabs[t])
+--     for w = 1, #tab_windows, 1 do
+--       table.insert(windows, tab_windows[w])
+--     end
+--   end
+-- end
+
 require("persisted").setup({
   autoload = true,
   allowed_dirs = allowed_dirs,
   ignored_dirs = ignored_dirs,
   telescope = {
     before_source = function()
+      -- Save session before switching
       local current_dir = vim.fn.getcwd()
       local is_included = utils.dir_list_includes(current_dir, allowed_dirs)
       local is_ignored = utils.dir_list_includes(current_dir, ignored_dirs)
       if (is_included and not is_ignored) then
         require 'persisted'.save()
       end
-    end
-  }
+      --  Close all buffers
+      vim.cmd("silent bufdo bwipeout")
+      -- Stop current LSPs
+      vim.lsp.stop_client(vim.lsp.get_active_clients())
+      -- Clear metals status line text
+      vim.g.metals_status = nil
+    end,
+  },
 })
 
 require('telescope').load_extension('persisted')
