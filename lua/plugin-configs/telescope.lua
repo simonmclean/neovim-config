1ea5ba8fb957e5c846b0891c09b065f0e90b8906
Simon McLean <simon.mclean@OVO-FVFGW22YQ05N.local>
2022-08-17 10:24:45 +0100

    telescope truncate paths

--- lua/plugin-configs/telescope.lua
+++ lua/plugin-configs/telescope.lua
@@ -3,3 +3,9 @@ local map = vim.api.nvim_set_keymap
 map('n', '<c-p>', ':Telescope find_files<cr>', {})
 map('n', '<c-f>', ':Telescope live_grep<cr>', {})
 map('n', '<c-s>', ':Telescope lsp_document_symbols<cr>', {})
+
+require'telescope'.setup {
+  defaults = {
+    path_display = { truncate = 2 }
+  }
+}
