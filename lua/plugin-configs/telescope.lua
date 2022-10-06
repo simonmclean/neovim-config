bbaa20ec0580e1f2c55a818996026482bc7ed60d
Simon McLean <simon.mclean@OVO-FVFGW22YQ05N.local>
2022-10-06 11:20:13 +0100

    telescope mapping

--- lua/plugin-configs/telescope.lua
+++ lua/plugin-configs/telescope.lua
@@ -2,7 +2,7 @@ local map = vim.api.nvim_set_keymap
 
 map('n', '<c-p>', ':Telescope find_files<cr>', {})
 map('n', '<c-f>', ':Telescope live_grep<cr>', {})
-map('n', '<c-s>', ':Telescope lsp_document_symbols<cr>', {})
+map('n', '<leader>s', ':Telescope lsp_document_symbols<cr>', {})
 
 require'telescope'.setup {
   defaults = {
