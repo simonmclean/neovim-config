f624ee8e4d953b9c91cb08bcf2086cc3b7a33133
Simon McLean <simon.mclean@192.168.1.131>
2023-07-17 11:11:51 +0100

    fix null-ls erroring when no eslint config exists

--- lua/plugin-configs/null-ls.lua
+++ lua/plugin-configs/null-ls.lua
@@ -4,12 +4,21 @@ return function()
   null_ls.setup({
     sources = {
       -- eslint
-      null_ls.builtins.code_actions.eslint_d,
-      null_ls.builtins.diagnostics.eslint_d,
-      null_ls.builtins.formatting.eslint_d,
+      null_ls.builtins.code_actions.eslint_d.with({
+        -- Setting only_local to prevent error message when there's no local eslint config
+        only_local = true
+      }),
+      null_ls.builtins.diagnostics.eslint_d.with({
+        only_local = true
+      }),
+      null_ls.builtins.formatting.eslint_d.with({
+        only_local = true
+      }),
 
       -- prettier
-      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.prettier.with({
+        only_local = true
+      }),
 
       -- clojure
       null_ls.builtins.formatting.cljstyle
