cccbf70fdf01202d1052b56af45f59b5a4975601
Simon McLean <simon.mclean@192.168.1.131>
2023-08-03 10:43:34 +0100

    <leader>t :Telescope

--- lua/plugin-configs/telescope.lua
+++ lua/plugin-configs/telescope.lua
@@ -4,6 +4,7 @@ return function()
   map('n', '<c-p>', ':Telescope find_files<cr>', {})
   map('n', '<c-f>', ':Telescope live_grep<cr>', {})
   map('n', '<leader>s', ':Telescope lsp_document_symbols<cr>', {})
+  map('n', '<leader>t', ':Telescope<cr>', {})
 
   require 'telescope'.setup {
     defaults = {
