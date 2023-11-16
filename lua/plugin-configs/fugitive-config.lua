70f284742bae302e2ef13b1d6e2cd59c28595aec
Simon McLean <simon.mclean@OVO-FVFGW22YQ05N.local>
2023-11-16 15:13:25 +0000

    FugitiveHook augroup

--- lua/plugin-configs/fugitive-config.lua
+++ lua/plugin-configs/fugitive-config.lua
@@ -1,8 +1,11 @@
 local u = require 'utils'
 
 return function()
+  local autocmd_group = vim.api.nvim_create_augroup('FugitiveHooks', {})
+
   vim.api.nvim_create_autocmd('User', {
     pattern = { 'FugitiveObject', 'FugitiveIndex' },
+    group = autocmd_group,
     callback = function()
       if u.is_git_repo() then
         u.update_git_status()
