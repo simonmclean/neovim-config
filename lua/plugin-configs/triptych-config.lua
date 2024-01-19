8f96e0d21981e1bb749748e5f4e54fe8e8ff6343
Simon McLean <simon.mclean@192.168.1.106>
2024-01-19 17:09:07 +0000

    update triptych config

--- lua/plugin-configs/triptych-config.lua
+++ lua/plugin-configs/triptych-config.lua
@@ -1,4 +1,15 @@
 return function()
-  require('triptych').setup()
+  require('triptych').setup {
+    extension_mappings = {
+      ['<c-f>'] = {
+        mode = 'n',
+        fn = function(target)
+          require('telescope.builtin').live_grep {
+            search_dirs = { target.path },
+          }
+        end,
+      },
+    },
+  }
   vim.keymap.set('n', '<leader>-', ':Triptych<CR>', { silent = true })
 end
