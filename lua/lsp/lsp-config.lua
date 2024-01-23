234a44a232afe8b5ff36183f46a62914acfac1b6
Simon McLean <simonmclean@192.168.1.101>
2024-01-23 22:23:34 +0000

    custom lsp hover handler

--- lua/lsp/lsp-config.lua
+++ lua/lsp/lsp-config.lua
@@ -96,3 +96,18 @@ for type, icon in pairs(signs) do
   local hl = 'DiagnosticSign' .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+-- Custom handler for hover. This acts like the default handler, except it won't print "No information available" when there's no hover info
+vim.lsp.handlers['textDocument/hover'] = function(_, result, ctx, config)
+  config = config or {}
+  config.focus_id = ctx.method
+  if not (result and result.contents) then
+    return
+  end
+  local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+  markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+  if vim.tbl_isempty(markdown_lines) then
+    return
+  end
+  return vim.lsp.util.open_floating_preview(markdown_lines, 'markdown', config)
+end
