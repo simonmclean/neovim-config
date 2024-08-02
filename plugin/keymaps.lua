8892619e02130b48db63d9156abfc304f9b7aacf
Simon McLean <simon.mclean@kaluza.com>
2024-08-02 14:58:52 +0100

    next/prev tab navigation

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -5,8 +5,13 @@ local silent = { silent = true }
 map('n', '|', ':vertical split<cr>', silent)
 map('n', '-', ':split<cr>', silent)
 
+-- Scroll
 map('n', '<C-k>', '3<C-y>', { desc = 'scroll up' })
 map('n', '<C-j>', '3<C-e>', { desc = 'scroll down' })
+
+-- Tab
+map('n', '<C-l>', 'gt', { desc = 'tab right' })
+map('n', '<C-h>', 'gT', { desc = 'tab left' })
 
 -- Lazy exec mode
 map('n', ';', ':')
