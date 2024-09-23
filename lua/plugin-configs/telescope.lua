6ac76764020a0b002f3723c4810de4d0197b1f65
Simon McLean <simon.mclean@kaluza.com>
2024-09-23 11:31:27 +0100

    telescope mapping for commands

--- lua/plugin-configs/telescope.lua
+++ lua/plugin-configs/telescope.lua
@@ -34,6 +34,7 @@ return {
     -- map('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     -- map('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
     map('n', '<leader>sb', grep_in_current_buffer, { desc = '[S]earch [B]uffer' })
+    map('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
 
     require('telescope').setup {
       defaults = {
