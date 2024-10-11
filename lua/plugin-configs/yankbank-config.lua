fdce908bae3ea811fd6165e92f9e37933de3effa
Simon McLean <simon.mclean@kaluza.com>
2024-10-11 10:25:58 +0100

    yankbank: include system clipboard

--- lua/plugin-configs/yankbank-config.lua
+++ lua/plugin-configs/yankbank-config.lua
@@ -3,6 +3,8 @@ return {
   config = function()
     require('yankbank').setup {
       num_behavior = 'jump',
+      -- Poll the unnamedplus register (aka system clipboard)
+      focus_gain_poll = true
     }
     vim.keymap.set('n', 'p', '<cmd>YankBank<CR>', { noremap = true })
   end,
