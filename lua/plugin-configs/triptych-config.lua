d4a060da15df36750126b5d3b786880d1fac03f1
Simon McLean <simonmclean@192.168.1.196>
2024-10-24 20:54:40 +0100

    update triptych config

--- lua/plugin-configs/triptych-config.lua
+++ lua/plugin-configs/triptych-config.lua
@@ -1,28 +1,25 @@
-local map = require 'utils'.keymap_set
-
 return {
   'simonmclean/triptych.nvim',
   event = 'VeryLazy',
-  -- dir = '~/code/triptych',
+  dir = '~/code/triptych',
   dev = false,
   dependencies = {
     'nvim-lua/plenary.nvim', -- required
     'nvim-tree/nvim-web-devicons', -- optional
   },
-  config = function()
-    require('triptych').setup {
-      extension_mappings = {
-        ['<leader>sg'] = {
-          mode = 'n',
-          fn = function(target)
-            require('telescope.builtin').live_grep {
-              search_dirs = { target.path },
-            }
-          end,
-        },
+  opts = {
+    extension_mappings = {
+      ['<leader>sg'] = {
+        mode = 'n',
+        fn = function(target)
+          require('telescope.builtin').live_grep {
+            search_dirs = { target.path },
+          }
+        end,
       },
-    }
-
-    map('n', '<leader>-', ':Triptych<CR>')
-  end,
+    },
+  },
+  keys = {
+    { '<leader>-', ':Triptych<CR>' },
+  },
 }
