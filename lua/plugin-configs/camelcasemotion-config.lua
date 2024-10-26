b336520dd491c85845b402b4161669b532d5f9b8
Simon McLean <simonmclean@192.168.1.196>
2024-10-27 00:16:38 +0100

    replace camcelcasemotion with spider

--- lua/plugin-configs/camelcasemotion-config.lua
+++
@@ -1,9 +0,0 @@
--- Motions for camelcase jumps
-
-return {
-  'bkad/camelcasemotion',
-  config = function()
-    vim.g.camelcasemotion_key = '<leader>'
-  end,
-  event = 'VeryLazy',
-}
---
+++ lua/plugin-configs/spider-config.lua
@@ -0,0 +1,10 @@
+return {
+  'chrisgrieser/nvim-spider',
+  opts = {},
+  event = 'VeryLazy',
+  keys = {
+    { 'w', "<cmd>lua require('spider').motion('w')<CR>", mode = { 'n', 'o', 'x' } },
+    { 'e', "<cmd>lua require('spider').motion('e')<CR>", mode = { 'n', 'o', 'x' } },
+    { 'b', "<cmd>lua require('spider').motion('b')<CR>", mode = { 'n', 'o', 'x' } },
+  },
+}
