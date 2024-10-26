7f30263252028c2c3e2330b9b2d858e2096d12f2
Simon McLean <simonmclean@192.168.1.196>
2024-10-27 00:33:28 +0100

    cleanup

--- lua/plugin-configs/actions-preview-config.lua
+++ lua/plugin-configs/actions-preview-config.lua
@@ -1,14 +1,12 @@
+local layout_config = require('plugin-configs.telescope').layout_config
+
 return {
   'aznhe21/actions-preview.nvim',
   event = 'VeryLazy',
   dependencies = { 'nvim-telescope/telescope.nvim' },
-  config = function()
-    local layout_config = require('plugin-configs.telescope').layout_config
-
-    require('actions-preview').setup {
-      telescope = {
-        layout_config = layout_config,
-      },
-    }
-  end,
+  opts = {
+    telescope = {
+      layout_config = layout_config,
+    },
+  },
 }
