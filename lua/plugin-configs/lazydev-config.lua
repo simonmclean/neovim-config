a09c7a26662d2bbbdc21525fff6b85dc4bea78ea
Simon McLean <simon.mclean@kaluza.com>
2024-10-29 10:15:06 +0000

    fix vim.loop typings

--- lua/plugin-configs/lazydev-config.lua
+++ lua/plugin-configs/lazydev-config.lua
@@ -9,7 +9,7 @@ return {
       library = {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+        { path = 'luvit-meta/library', words = { 'vim%.uv', 'vim%.loop' } },
       },
     },
   },
