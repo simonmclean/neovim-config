ab15e0b53516c066e70d712b042c2fdd6b820f5f
Simon McLean <simonmclean@simons-mbp-2.home>
2024-11-17 14:56:22 +0000

    add luacheckrc

--- lua/lsp/language-servers/lua.lua
+++ lua/lsp/language-servers/lua.lua
@@ -1,6 +1,12 @@
 return {
   settings = {
     Lua = {
+      workspace = {
+        ignoreDir = { '.luacheckrc' },
+      },
+      diagnostics = {
+        disable = { '.luacheckrc' },
+      },
       runtime = {
         version = 'LuaJIT',
       },
