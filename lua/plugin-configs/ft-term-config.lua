a201e115859907bd2df706c14ece2183654893ff
Simon McLean <simon.mclean@kaluza.com>
2024-12-03 13:55:19 +0000

    improve FTerm key bindings

--- lua/plugin-configs/ft-term-config.lua
+++ lua/plugin-configs/ft-term-config.lua
@@ -1,12 +1,14 @@
 -- floating terminal plugin for neovim
 
+---@module 'lazy'
+---@type LazyPluginSpec
 return {
   'numToStr/FTerm.nvim',
   event = 'VeryLazy',
   opts = {
-    blend = vim.g.winblend
+    blend = vim.g.winblend,
   },
   keys = {
-    { '<c-t>', '<cmd>lua require("FTerm").toggle()<CR>', desc = 'Toggle floating terminal' },
+    { '<c-t>', '<cmd>lua require("FTerm").toggle()<CR>', desc = 'Toggle floating terminal', mode = { 'n', 't' } },
   },
 }
