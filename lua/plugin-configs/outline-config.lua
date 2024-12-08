12b9e8dd684c4246df00265a2ba67aa3fdb1e4c9
Simon McLean <simonmclean@192.168.1.105>
2024-12-08 14:10:24 +0000

    outline config updates

--- lua/plugin-configs/outline-config.lua
+++ lua/plugin-configs/outline-config.lua
@@ -1,10 +1,47 @@
 -- A sidebar with a tree-like outline of symbols from your code, powered by LSP.
 
+-- https://github.com/hedyhli/outline.nvim/blob/main/lua/outline/symbols.lua
+local icons = {
+  Function = 'func',
+  Class = 'class',
+  Method = 'method',
+  Property = 'prop',
+  Constructor = 'constructor',
+  Variable = 'var',
+  Constant = 'const',
+  String = 'str',
+  Number = 'num',
+  Boolean = 'bool',
+  Array = 'arr',
+  Object = 'obj',
+}
+
+local function icon_fetcher(kind)
+  if icons[kind] then
+    return icons[kind]
+  end
+  return string.lower(kind:sub(1, 4))
+end
+
 return {
   'hedyhli/outline.nvim',
   cmd = { 'Outline', 'OutlineOpen' },
   keys = {
-    { '<leader>o', '<cmd>Outline<CR>', desc = 'Toggle outline' },
+    -- Note: The exclamation mark means "open but don't focus"
+    { '<leader>o', '<cmd>Outline!<CR>', desc = 'Toggle outline' },
   },
-  opts = {},
+  opts = {
+    preview_window = {
+      auto_preview = true,
+      winblend = vim.g.winblend,
+    },
+    outline_window = {
+      -- These 2 options blend cursor with cursorline
+      show_cursorline = true,
+      hide_cursor = true,
+    },
+    symbols = {
+      icon_fetcher = icon_fetcher
+    },
+  },
 }
