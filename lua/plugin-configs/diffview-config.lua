d92ef245a06ca0a0905ed09d87d08377d64de027
Simon McLean <simon.mclean@kaluza.com>
2024-12-19 15:10:56 +0000

    VimLeavePre close diffview

--- lua/plugin-configs/diffview-config.lua
+++ lua/plugin-configs/diffview-config.lua
@@ -1,6 +1,21 @@
 ---@diagnostic disable: missing-fields
 -- Advanced git diff UI
 
+-- Close diffview before exiting, otherwise it makes a mess when the session is restored
+vim.api.nvim_create_autocmd('VimLeavePre', {
+  callback = function()
+    local diffview_installed, _ = pcall(require, 'diffview')
+    if diffview_installed then
+      local views = require('diffview.lib').views
+      if #views > 0 then
+        for _, view in ipairs(views) do
+          view:close()
+        end
+      end
+    end
+  end,
+})
+
 ---@module 'lazy'
 ---@type LazyPluginSpec
 return {
