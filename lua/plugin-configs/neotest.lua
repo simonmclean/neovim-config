221c286e040988391640c28c9a9084a7ae93eb34
Simon McLean <simon.mclean@kaluza.com>
2025-02-20 11:37:02 +0000

    close neotest windows on VimLeavePre

--- lua/plugin-configs/neotest.lua
+++ lua/plugin-configs/neotest.lua
@@ -17,5 +17,19 @@ return {
         require 'neotest-vitest',
       },
     }
+
+    -- Close neotest before exiting, otherwise it makes a mess when the session is restored
+    vim.api.nvim_create_autocmd('VimLeavePre', {
+      callback = function()
+        local wins = vim.api.nvim_list_wins()
+        for _, win in ipairs(wins) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          local ft = vim.api.nvim_get_option_value('filetype', { buf = buf })
+          if ft and string.find(ft, 'neotest') then
+            vim.api.nvim_win_close(win, true)
+          end
+        end
+      end,
+    })
   end,
 }
