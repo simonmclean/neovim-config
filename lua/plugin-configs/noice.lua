5bcfd8ed54bc7e6aaf41ac0277d20f3c120c24eb
Simon McLean <simonmclean@192.168.1.231>
2025-03-29 15:25:53 +0000

    disable some noice things

--- lua/plugin-configs/noice.lua
+++ lua/plugin-configs/noice.lua
@@ -8,9 +8,9 @@ return {
     lsp = {
       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
       override = {
-        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-        ['vim.lsp.util.stylize_markdown'] = true,
-        ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = false,
+        ['vim.lsp.util.stylize_markdown'] = false,
+        ['cmp.entry.get_documentation'] = false, -- requires hrsh7th/nvim-cmp
       },
       hover = {
         silent = true,
@@ -34,7 +34,7 @@ return {
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = true, -- add a border to hover docs and signature help
+      lsp_doc_border = false, -- add a border to hover docs and signature help
     },
   },
 }
