21658ea762c4faaac4b82ee52134acae0d001833
Simon McLean <simon.mclean@kaluza.com>
2025-04-09 09:25:43 +0100

    blink-cmp-copilot

--- lua/plugin-configs/blink.lua
+++ lua/plugin-configs/blink.lua
@@ -1,20 +1,30 @@
 return {
   'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = { 'rafamadriz/friendly-snippets', 'giuxtaposition/blink-cmp-copilot' },
   version = '1.*',
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
+    sources = {
+      default = { 'lsp', 'copilot', 'path', 'snippets', 'buffer' },
+      providers = {
+        copilot = {
+          name = 'copilot',
+          module = 'blink-cmp-copilot',
+          score_offset = 100,
+          async = true,
+        },
+      },
+    },
     completion = { documentation = { auto_show = true } },
     signature = { enabled = true },
     cmdline = {
       completion = {
         menu = {
-          auto_show = true
-        }
-      }
-    }
+          auto_show = true,
+        },
+      },
+    },
   },
   opts_extend = { 'sources.default' },
 }
