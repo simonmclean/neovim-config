6395b7fa5fbefe7f62b961a5d9fe25a4b7dacad1
Simon McLean <simon.mclean@kaluza.co>
2025-04-25 19:41:45 +0100

    update mini surround config

--- lua/plugin-configs/mini.lua
+++ lua/plugin-configs/mini.lua
@@ -30,10 +30,15 @@ return {
     version = '*',
     event = 'VeryLazy',
     opts = {
+      n_lines = 50,
       mappings = {
         add = 'gs', -- Add surrounding in Normal and Visual modes
         delete = 'ds', -- Delete surrounding
         replace = 'cs', -- Replace surrounding
+        find = '', -- Find surrounding (to the right)
+        find_left = '', -- Find surrounding (to the left)
+        highlight = '', -- Highlight surrounding
+        update_n_lines = '', -- Update `n_lines`
       },
     },
   },
