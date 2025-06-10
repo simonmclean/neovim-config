19b6fe867b862ee9536cb7725a4e44bb7fddcc74
Simon McLean <simon.mclean@kaluza.co>
2025-06-10 10:50:25 +1000

    update bufferline config

--- lua/plugin-configs/bufferline.lua
+++ lua/plugin-configs/bufferline.lua
@@ -1,11 +1,17 @@
+-- Convert tabline into a buffer line + tabline
+
 return {
   'akinsho/bufferline.nvim',
   version = '*',
   event = 'VeryLazy',
-  dependencies = 'nvim-tree/nvim-web-devicons',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'tiagovla/scope.nvim'
+  },
   opts = {},
   keys = {
     { '<C-l>', ':BufferLineCycleNext<CR>', silent = true },
     { '<C-h>', ':BufferLineCyclePrev<CR>', silent = true },
+    { '<C-q>', ':bdelete<CR>', silent = true },
   },
 }
