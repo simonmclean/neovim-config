15b50d7f8a5f4b98e0b027955255e69818298cda
Simon McLean <simon.mclean@kaluza.co>
2025-11-14 13:53:38 +0000

    switch to treesitter main branch

--- lua/plugin-configs/treesitter.lua
+++ lua/plugin-configs/treesitter.lua
@@ -1,23 +1,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  dependencies = { 'OXY2DEV/markview.nvim' },
+  branch = 'main',
   lazy = false,
-  config = function()
-    require('nvim-treesitter.configs').setup {
-      indent = {
-        enable = true, -- without this JSX auto-indentation doesn't work as expected
-      },
-      highlight = {
-        enable = true,
-        custom_captures = {
-          -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
-          -- ["foo.bar"] = "Identifier",
-        },
-      },
-      auto_install = true,
-    }
-
-    vim.treesitter.language.register('terraform', 'terraform-vars')
-  end,
 }
